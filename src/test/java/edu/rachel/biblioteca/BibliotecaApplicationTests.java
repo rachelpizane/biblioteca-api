cafc3b841c96100791fa51399331f808d7c34c05
Rachel Pizane <rachel.maia@dbserver.com.br>
2026-03-03 14:21:03 -0300

    chore: iniciar projeto

---
+++ src/test/java/edu/rachel/biblioteca/BibliotecaApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.rachel.biblioteca;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BibliotecaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
