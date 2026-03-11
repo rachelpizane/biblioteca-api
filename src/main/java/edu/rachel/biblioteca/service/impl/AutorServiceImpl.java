879328596c11f54af06307286589a4f5052553b7
Rachel Pizane <rachel.maia@dbserver.com.br>
2026-03-11 15:26:00 -0300

    feat: implementar busca de um autor pelo id

--- src/main/java/edu/rachel/biblioteca/service/impl/AutorServiceImpl.java
+++ src/main/java/edu/rachel/biblioteca/service/impl/AutorServiceImpl.java
@@ -2,6 +2,7 @@ package edu.rachel.biblioteca.service.impl;
 
 import edu.rachel.biblioteca.dto.AutorRequestDTO;
 import edu.rachel.biblioteca.dto.AutorResponseDTO;
+import edu.rachel.biblioteca.exception.NotFoundException;
 import edu.rachel.biblioteca.mapper.AutorMapper;
 import edu.rachel.biblioteca.model.Autor;
 import edu.rachel.biblioteca.repository.AutorRepository;
@@ -9,6 +10,8 @@ import edu.rachel.biblioteca.service.AutorService;
 import edu.rachel.biblioteca.validator.AutorValidator;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.UUID;
 
 @AllArgsConstructor
 @Service
@@ -23,5 +26,13 @@ public class AutorServiceImpl implements AutorService {
         Autor autorSalvo = repository.save(mapper.paraEntidade(request));
 
         return mapper.paraDto(autorSalvo);
+    }
+
+    @Override
+    public AutorResponseDTO buscarAutor(UUID id) {
+        return repository
+                .findById(id)
+                .map(mapper::paraDto)
+                .orElseThrow(() -> new NotFoundException("Autor"));
     }
 }
