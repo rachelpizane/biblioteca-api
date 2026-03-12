bf3311f14229354bcf9b4bc006ae056818644a3f
Rachel Pizane <rachel.maia@dbserver.com.br>
2026-03-12 17:29:01 -0300

    test: incluir testes de integração e unitários para cadastramento do locatário

---
+++ src/test/java/edu/rachel/biblioteca/service/LocatarioServiceImplTest.java
@@ -0,0 +1,88 @@
+package edu.rachel.biblioteca.service;
+
+import edu.rachel.biblioteca.dto.LocatarioRequestDTO;
+import edu.rachel.biblioteca.dto.LocatarioResponseDTO;
+import edu.rachel.biblioteca.exception.BusinessException;
+import edu.rachel.biblioteca.mapper.LocatarioMapper;
+import edu.rachel.biblioteca.mock.LocatarioMock;
+import edu.rachel.biblioteca.model.Locatario;
+import edu.rachel.biblioteca.repository.LocatarioRepository;
+import edu.rachel.biblioteca.service.impl.LocatarioServiceImpl;
+import edu.rachel.biblioteca.validator.LocatarioValidator;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mapstruct.factory.Mappers;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.UUID;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.never;
+
+@ExtendWith(MockitoExtension.class)
+class LocatarioServiceImplTest {
+    @Mock
+    LocatarioRepository repository;
+
+    LocatarioMapper mapper;
+
+    LocatarioValidator validator;
+
+    LocatarioServiceImpl service;
+
+    @BeforeEach
+    void setUp() {
+        mapper =  Mappers.getMapper(LocatarioMapper.class);
+        validator = new LocatarioValidator(repository);
+        service = new LocatarioServiceImpl(validator, mapper, repository);
+    }
+
+    @Nested
+    class CadastrarLocatarioTests {
+        @Test
+        void deveSalvarLocatarioCorretamente(){
+            LocatarioRequestDTO request = LocatarioMock.getLocatarioRequestDTOMock();
+            Locatario locatario = LocatarioMock.getLocatarioMock(UUID.randomUUID());
+
+            when(repository.save(any(Locatario.class))).thenReturn(locatario);
+
+            LocatarioResponseDTO response = service.cadastrarLocatario(request);
+
+            assertEquals(response.id(), locatario.getId());
+            verify(repository, times(1)).save(any(Locatario.class));
+        }
+
+        @Test
+        void deveLancarBusinessExceptionQuandoExistirLocatarioComCPF(){
+            LocatarioRequestDTO request = LocatarioMock.getLocatarioRequestDTOMock();
+
+            when(repository.existsByCpf(request.cpf())).thenReturn(true);
+
+            assertThrows(BusinessException.class, () -> {
+                service.cadastrarLocatario(request);
+            });
+
+            verify(repository, never()).save(any(Locatario.class));
+        }
+
+        @Test
+        void deveLancarBusinessExceptionQuandoExistirLocatarioComEmail(){
+            LocatarioRequestDTO request = LocatarioMock.getLocatarioRequestDTOMock();
+
+            when(repository.existsByCpf(request.cpf())).thenReturn(false);
+            when(repository.existsByEmailIgnoreCase(request.email())).thenReturn(true);
+
+            assertThrows(BusinessException.class, () -> {
+                service.cadastrarLocatario(request);
+            });
+
+            verify(repository, never()).save(any(Locatario.class));
+        }
+    }
+}
