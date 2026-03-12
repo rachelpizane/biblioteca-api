0f511236415299b224d5834ea603e23c8d9f5751
Rachel Pizane <rachel.maia@dbserver.com.br>
2026-03-12 13:42:45 -0300

    test: incluir testes de integração e unitários para cadastramento do livro

--- src/main/java/edu/rachel/biblioteca/utils/JsonUtils.java
+++ src/main/java/edu/rachel/biblioteca/utils/JsonUtils.java
@@ -2,10 +2,13 @@ package edu.rachel.biblioteca.utils;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 
 public class JsonUtils {
-    private static final ObjectMapper objectMapper = new ObjectMapper().registerModule(new JavaTimeModule());
+    private static final ObjectMapper objectMapper = new ObjectMapper()
+            .registerModule(new JavaTimeModule())
+            .disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
 
     public static String convertToJson(Object object) {
         try {
---
+++ src/test/java/edu/rachel/biblioteca/service/LivroServiceImplTest.java
@@ -0,0 +1,103 @@
+package edu.rachel.biblioteca.service;
+
+import edu.rachel.biblioteca.dto.LivroRequestDTO;
+import edu.rachel.biblioteca.dto.LivroResponseDTO;
+import edu.rachel.biblioteca.exception.BusinessException;
+import edu.rachel.biblioteca.exception.NotFoundException;
+import edu.rachel.biblioteca.mapper.AutorMapper;
+import edu.rachel.biblioteca.mapper.LivroMapper;
+import edu.rachel.biblioteca.mock.AutorMock;
+import edu.rachel.biblioteca.mock.LivroMock;
+import edu.rachel.biblioteca.model.Autor;
+import edu.rachel.biblioteca.model.Livro;
+import edu.rachel.biblioteca.repository.AutorRepository;
+import edu.rachel.biblioteca.repository.LivroRepository;
+import edu.rachel.biblioteca.service.impl.LivroServiceImpl;
+import edu.rachel.biblioteca.validator.LivroValidator;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mapstruct.factory.Mappers;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.test.util.ReflectionTestUtils;
+
+import java.util.List;
+import java.util.UUID;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+class LivroServiceImplTest {
+    @Mock
+    AutorRepository autorRepository;
+
+    @Mock
+    LivroRepository livroRepository;
+
+    AutorMapper autorMapper;
+
+    LivroMapper livroMapper;
+
+    LivroValidator validator;
+
+    LivroServiceImpl service;
+
+    @BeforeEach
+    void setUp() {
+        autorMapper = Mappers.getMapper(AutorMapper.class);
+        livroMapper = Mappers.getMapper(LivroMapper.class);
+        ReflectionTestUtils.setField(livroMapper, "autorMapper", autorMapper);
+
+        validator = new LivroValidator(livroRepository, autorRepository);
+        service = new LivroServiceImpl(validator, livroMapper, autorRepository, livroRepository);
+    }
+
+    @Nested
+    class CadastrarLivroTests {
+        @Test
+        void deveSalvarLivroCorretamente(){
+            Autor autor = AutorMock.getAutorMock(UUID.randomUUID());
+            LivroRequestDTO request = LivroMock.getLivroRequestDTOMock(List.of(autor.getId()));
+            Livro livro = LivroMock.getLivroMock(UUID.randomUUID(), autor.getId());
+
+            when(autorRepository.findAllById(request.autoresIds())).thenReturn(List.of(autor));
+            when(livroRepository.save(any(Livro.class))).thenReturn(livro);
+
+            LivroResponseDTO response = service.cadastrarLivro(request);
+
+            assertEquals(livro.getId(), response.id());
+            assertEquals(1, response.autores().size());
+            assertEquals(autor.getId(), response.autores().getFirst().id());
+            verify(livroRepository, times(1)).save(any(Livro.class));
+        }
+
+        @Test
+        void deveLancarBusinessExceptionQuandoExistirLivroComIsbn(){
+            LivroRequestDTO request = LivroMock.getLivroRequestDTOMock(List.of(UUID.randomUUID()));
+
+            when(livroRepository.existsByIsbn(request.isbn())).thenReturn(true);
+
+            assertThrows(BusinessException.class, () -> {
+                service.cadastrarLivro(request);
+            });
+            verify(livroRepository, never()).save(any(Livro.class));
+        }
+
+        @Test
+        void deveLancarNotFoundExceptionQuandoExistirLivroComIsbn(){
+            List<UUID> autoresIds = List.of(UUID.randomUUID());
+            LivroRequestDTO request = LivroMock.getLivroRequestDTOMock(autoresIds);
+
+            when(autorRepository.findAllById(autoresIds)).thenReturn(List.of());
+
+            assertThrows(NotFoundException.class, () -> {
+                service.cadastrarLivro(request);
+            });
+            verify(livroRepository, never()).save(any(Livro.class));
+        }
+    }
+}
