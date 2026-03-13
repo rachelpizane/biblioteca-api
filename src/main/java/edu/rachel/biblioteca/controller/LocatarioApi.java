d6be791764c5f8c5f359d157f8b1bf4f587efb4d
Rachel Pizane <rachel.maia@dbserver.com.br>
2026-03-13 09:40:14 -0300

    feat: implementar busca de locatário por id

--- src/main/java/edu/rachel/biblioteca/controller/LocatarioApi.java
+++ src/main/java/edu/rachel/biblioteca/controller/LocatarioApi.java
@@ -1,7 +1,7 @@
 package edu.rachel.biblioteca.controller;
 
-import edu.rachel.biblioteca.dto.LocatarioRequestDTO;
 import edu.rachel.biblioteca.dto.LocatarioResponseDTO;
+import edu.rachel.biblioteca.dto.LocatarioRequestDTO;
 import edu.rachel.biblioteca.dto.ErrorResponseDTO;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
@@ -10,9 +10,9 @@ import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
 
 @Tag(
         name = "Locatários",
@@ -41,4 +41,25 @@ public interface LocatarioApi {
     )
     @PostMapping
     ResponseEntity<LocatarioResponseDTO> cadastrarLocatario(@Valid @RequestBody(required = true) LocatarioRequestDTO request);
+
+    @Operation(
+            summary = "Buscar um locatário",
+            description = "Busca um locatário pelo seu id"
+    )
+    @ApiResponse(
+            responseCode = "200",
+            description = "Locatário buscado com sucesso",
+            content = @Content(
+                    schema = @Schema(implementation = LocatarioResponseDTO.class)
+            )
+    )
+    @ApiResponse(
+            responseCode = "404",
+            description = "Locatário não encontrado",
+            content = @Content(
+                    schema = @Schema(implementation = ErrorResponseDTO.class)
+            )
+    )
+    @GetMapping("/{id}")
+    ResponseEntity<LocatarioResponseDTO> buscarLocatario(@PathVariable UUID id);
 }
--- src/main/java/edu/rachel/biblioteca/service/LocatarioService.java
+++ src/main/java/edu/rachel/biblioteca/service/LocatarioService.java
@@ -2,6 +2,9 @@ package edu.rachel.biblioteca.service;
 
 import edu.rachel.biblioteca.dto.*;
 
+import java.util.UUID;
+
 public interface LocatarioService {
     LocatarioResponseDTO cadastrarLocatario(LocatarioRequestDTO request);
+    LocatarioResponseDTO buscarLocatario(UUID id);
 }
